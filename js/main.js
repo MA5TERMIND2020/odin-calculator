da7b17f4be3859678d3ae2c2ccfb0b1ac9c2c847
MA5TERMIND2020 <77905424+MA5TERMIND2020@users.noreply.github.com>
2023-01-02 06:45:37 -0500

    Display the current number on the screen

--- js/main.js
+++ js/main.js
@@ -1,25 +1,30 @@
 //Set some Global variables
-let operator = "";
-let previousValue = "";
-let currentValue = "";
+let operator = '';
+let previousValue = '';
+let currentValue = '';
 
 //Create a reference to the DOM elements that we want to work with
 const clear = document.querySelector('.clear');
 const backspace = document.querySelector('.backspace');
-const numbers = document.querySelectorAll(".number");
+const numbers = document.querySelectorAll('.number');
 const dot = document.querySelector('.dot');
 const operators = document.querySelector('.operator');
 const equals = document.querySelector('.equals');
 const previousScreen = document.querySelector('.previous');
-const current = document.querySelector('.current');
+const currentScreen = document.querySelector('.current');
 
 
-//Add an Event Listener to the Number buttons
-numbers.forEach((item) => item.addEventListener("click", function(e) {
+/*Add an Event Listener to the Number buttons that updates the currentValue variable
+ and displays it on the screen if it is not longer than 10 digits.*/
+numbers.forEach(item => item.addEventListener('click', (e) => {
     handleNumber(e.target.textContent);
+    currentScreen.textContent = currentValue;
 }))
 
 function handleNumber(num) {
+    if (currentValue.length <= 10) {
+    currentValue += num;
     console.log(num);
+    }
 }
 

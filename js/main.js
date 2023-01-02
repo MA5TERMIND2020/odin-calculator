fcae9ed9fae30fbb0f3d09fcabb35500ab3fd310
MA5TERMIND2020 <77905424+MA5TERMIND2020@users.noreply.github.com>
2023-01-02 07:26:44 -0500

    add an Event Listener to the Operator buttons and implement logic to update the Global variables and screen accordingly

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ const clear = document.querySelector('.clear');
 const backspace = document.querySelector('.backspace');
 const numbers = document.querySelectorAll('.number');
 const dot = document.querySelector('.dot');
-const operators = document.querySelector('.operator');
+const operators = document.querySelectorAll('.operator');
 const equals = document.querySelector('.equals');
 const previousScreen = document.querySelector('.previous');
 const currentScreen = document.querySelector('.current');
@@ -17,14 +17,21 @@ const currentScreen = document.querySelector('.current');
 /*Add an Event Listener to the Number buttons that updates the currentValue variable
  and displays it on the screen if it is not longer than 10 digits.*/
 numbers.forEach(item => item.addEventListener('click', (e) => {
-    handleNumber(e.target.textContent);
+    if (currentValue.length <= 10) {
+        currentValue += e.target.textContent;
     currentScreen.textContent = currentValue;
+    }
 }))
 
-function handleNumber(num) {
-    if (currentValue.length <= 10) {
-    currentValue += num;
-    console.log(num);
+/*Add an Event Listener to the Operator buttons, so that if there is no value already in operator, it updates the operator variable, sets the
+ previousValue equal to the currentValue, sets the previousScreen.textContent to the previousValue and the operator, sets the
+ currentScreen.textContent equal to the currentValue, and finally empties the contents of currentValue to get ready for new input.*/
+operators.forEach(item => item.addEventListener('click', (e) => {
+    if (operator == 0) {
+    operator = e.target.textContent;
+    previousValue = currentValue;
+    previousScreen.textContent = previousValue + '' + operator;
+    currentScreen.textContent = currentValue;
+    currentValue = '';
     }
-}
-
+}))

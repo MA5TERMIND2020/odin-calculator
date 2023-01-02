eb9646a861f2cfffe9221dd5247c2a6365c443a3
MA5TERMIND2020 <77905424+MA5TERMIND2020@users.noreply.github.com>
2023-01-02 17:15:45 -0500

    Add an Event Listener to the Backspace button

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ let currentValue = '';
 
 //Create a reference to the DOM elements that we want to work with
 const clear = document.querySelector('.clear');
-const backspace = document.querySelector('.backspace');
+const backspace = document.querySelector('.back');
 const numbers = document.querySelectorAll('.number');
 const decimal = document.querySelector('.dot');
 const operators = document.querySelectorAll('.operator');
@@ -23,6 +23,7 @@ numbers.forEach(item => item.addEventListener('click', (e) => {
     }
 }))
 
+//Add an Event Listener to the Decimal button, so that as long as no decimal has already been used, it will add the decimal to the currentValue."
 decimal.addEventListener('click', (e) => {
     if (!currentValue.includes('.')) {
         currentValue += '.';
@@ -33,12 +34,13 @@ decimal.addEventListener('click', (e) => {
  previousValue equal to the currentValue, sets the previousScreen.textContent to the previousValue and the operator, sets the
  currentScreen.textContent equal to the currentValue, and finally empties the contents of currentValue to get ready for new input.*/
 operators.forEach(item => item.addEventListener('click', (e) => {
-    if (operator == 0) {
+    if (!currentValue.includes('-' || '+' || '*' || '/') && !previousValue.includes('-' || '+' || '*' || '/')) {
     operator = e.target.textContent;
     previousValue = currentValue;
-    previousScreen.textContent = previousValue + '' + operator;
-    currentScreen.textContent = currentValue;
+    previousScreen.textContent += previousValue + ' ' + operator;
     currentValue = '';
+    currentScreen.textContent = currentValue;
+    
     }
 }))
 
@@ -49,6 +51,13 @@ clear.addEventListener('click', () => {
     operator = '';
     previousScreen.textContent = '';
     currentScreen.textContent = '';
+})
+
+/*Add an Event Listener for the Backspace button.*/
+backspace.addEventListener('click', (e) => {
+    
+    currentValue = currentValue.slice(0, -1);
+    currentScreen.textContent = currentValue;
 })
 
 /*Add an Event Listener to the Equals button that calculates the expression and then displays the result on the screen as long as it is 15
